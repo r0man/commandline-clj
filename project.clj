8655491459eeb515aca67e4a46629772b33595d8
Roman <roman@burningswell.com>
2012-03-13 21:37:15 +0100

    Get ready for Leiningen 2.

--- project.clj
+++ project.clj
@@ -1,11 +1,7 @@
-(defproject commandline-clj "0.1.1-SNAPSHOT"
+(defproject commandline-clj/commandline-clj "0.1.1-SNAPSHOT"
   :description "Clojure command line parsing library."
-  :autodoc {:name "Clojure command line parsing library."
-            :author "Roman Scherer"
-            :web-src-dir "http://github.com/r0man/commandline-clj/blob/"
-            :web-home "http://github.com/r0man/commandline-clj"
-            :copyright "Copyright (c) 2011 Roman Scherer"}
   :url "https://github.com/r0man/commandline-clj"
   :dependencies [[commons-cli/commons-cli "1.2"]
                  [clj-time "0.3.7"]
-                 [org.clojure/clojure "1.4.0-beta4"]])
+                 [org.clojure/clojure "1.4.0-beta4"]]
+  :min-lein-version "2.0.0")
