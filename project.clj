ef71cbee29162c558d3ded9da5968be70f01db2c
Roman <roman@burningswell.com>
2012-04-16 19:55:44 +0200

    Switched to Clojure 1.4.0.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject commandline-clj/commandline-clj "0.1.1-SNAPSHOT"
   :description "Clojure command line parsing library."
   :url "https://github.com/r0man/commandline-clj"
-  :dependencies [[commons-cli/commons-cli "1.2"]
-                 [clj-time "0.3.7"]
-                 [org.clojure/clojure "1.4.0-beta4"]]
+  :dependencies [[clj-time "0.4.0"]
+                 [commons-cli/commons-cli "1.2"]
+                 [org.clojure/clojure "1.4.0"]]
   :min-lein-version "2.0.0")
