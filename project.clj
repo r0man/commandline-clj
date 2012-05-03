479d8be1a0feaf594961717222322efed0d6b347
Roman <roman@burningswell.com>
2012-05-03 23:36:29 +0200

    Started 0.1.3-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject commandline-clj/commandline-clj "0.1.2"
+(defproject commandline-clj/commandline-clj "0.1.3-SNAPSHOT"
   :description "Clojure command line parsing library."
   :min-lein-version "2.0.0"
   :url "https://github.com/r0man/commandline-clj"
-  :dependencies [[clj-time "0.4.0"]
+  :dependencies [[clj-time "0.4.2"]
                  [commons-cli/commons-cli "1.2"]
                  [org.clojure/clojure "1.4.0"]])
