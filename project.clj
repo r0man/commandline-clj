0a566bb343659bdecdbfd2ab964bf5b97ad34634
roman <roman@bombaclaat.lan>
2012-07-27 19:48:24 +0200

    Switched to clj-time 0.4.4.

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Clojure command line parsing library."
   :min-lein-version "2.0.0"
   :url "https://github.com/r0man/commandline-clj"
-  :dependencies [[clj-time "0.4.2"]
+  :dependencies [[clj-time "0.4.4"]
                  [commons-cli/commons-cli "1.2"]
                  [org.clojure/clojure "1.4.0"]])
