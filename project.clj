0c7f3f8695260fc96a66eaa82e6442b4441c787b
Roman Scherer <roman.scherer@burningswell.com>
2013-01-06 17:50:55 +0100

    Added license.

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :description "Clojure command line parsing library."
   :min-lein-version "2.0.0"
   :url "https://github.com/r0man/commandline-clj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clj-time "0.4.4"]
                  [commons-cli/commons-cli "1.2"]
                  [org.clojure/clojure "1.4.0"]])
