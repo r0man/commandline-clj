0472c1884aa9762ad942b70c6fb8fcbb0bb9b2f4
Roman Scherer <roman.scherer@burningswell.com>
2013-04-08 20:51:35 +0200

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :url "https://github.com/r0man/commandline-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[clj-time "0.4.4"]
+  :dependencies [[clj-time "0.5.0"]
                  [commons-cli/commons-cli "1.2"]
-                 [org.clojure/clojure "1.5.0"]])
+                 [org.clojure/clojure "1.5.1"]])
